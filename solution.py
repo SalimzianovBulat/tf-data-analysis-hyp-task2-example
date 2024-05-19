d213a55e59929dd63f20ce61716b0646f4f0240f
Bul11k <139507357+SalimzianovBulat@users.noreply.github.com>
2024-05-19 22:43:12 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,9 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 469801461 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    import scipy.stats as sps
+    alpha = 0.05
+    return sps.ks_2samp(np.ones(3), np.zeros(3))[1] <= alpha # Ваш ответ, True или False
